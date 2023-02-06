20f9ca3e59202e017dc3341d28b644eb8bac2747
madisonmeccia <madisonnicole317@gmail.com>
2023-02-06 13:56:11 -0500

    j

---
+++ index.js
@@ -0,0 +1,21 @@
+let c=0
+let d=0
+let q=0
+let n=5
+
+while (N>25) {
+    text+="the change is" -25;
+    N--;
+    
+    if N>25
+    else use quarters;
+    
+    if 25>N>10
+    else use dime;  
+    
+    if 10>N>5
+    else use nickels;  
+    
+    if 5>N>1
+    else use pennies;
+}
